e45cdc53b2551eec997fc394724cd422694ce5e8
Sofia Bravo <sofiabravo103@gmail.com>
2017-03-21 19:50:38 -0400

    added models for queries app

--- queries/models.py
+++ queries/models.py
@@ -1,1 +1,38 @@
 from django.db import models
+from taggit.managers import TaggableManager
+
+
+class Query(models.Model):
+    OFFER = 'OF'
+    REQUEST = 'RE'
+    INTENTION_CHOICES = (
+        (OFFER, 'Offer'),
+        (REQUEST, 'Request')
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    intention = models.CharField(
+        max_length=2,
+        choices=INTENTION_CHOICES
+    )
+    intersect_keywords = models.BooleanField(default=True)
+    keywords = TaggableManager()
+
+
+class Execution(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    auto = models.BooleanField(default=False)
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+
+
+class Result(models.Model):
+    text_hash = models.CharField(max_length=32, min_length=32)
+    from_twitter_status_id = models.CharField()
+    useful = models.BooleanField(default=False)
+    execution = models.ForeignKey(Execution, on_delete=models.CASCADE)
+    #source = models.ForeignKey(Source, on_delete=models.PROTECT)
+
+
+class Alert(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)

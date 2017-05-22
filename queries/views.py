dddc4c76cf4694cfb20c836693a1b01daf6ddfda
Ana <citricn@gmail.com>
2017-05-22 22:44:37 +0100

    Added serializer and views.

--- queries/views.py
+++ queries/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from queries.models import Query
+from queries.serializers import QuerySerializer
 
 # Create your views here.
+@csrf_exempt
+def query_list(request):
+    """
+    List all code queries, or create a new query.
+    """
+    if request.method == 'GET':
+        queries = Query.objects.all()
+        serializer = QuerySerializer(queries, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = QuerySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def query_detail(request, pk):
+    """
+    Retrieve, update or delete a code query.
+    """
+    try:
+        query = Query.objects.get(pk=pk)
+    except Query.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = QuerySerializer(query)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = QuerySerializer(query, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        query.delete()
+        return HttpResponse(status=204)

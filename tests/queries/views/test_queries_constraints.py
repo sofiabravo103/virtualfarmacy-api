bbf2964d4c37771c7a626f2a107a516ed19ad5a1
Sofia Bravo <sofiabravo103@gmail.com>
2017-09-10 15:05:49 +0200

    Added additional constraint test for query model

--- tests/queries/views/test_queries_constraints.py
+++ tests/queries/views/test_queries_constraints.py
@@ -8,7 +8,7 @@ from tests.factories import QueryFactory, random_keyword_set
 
 
 @pytest.mark.django_db
-def test_incorrenct_intention_choice(client):
+def test_bad_request_with_inexisting_intention_choice(client):
     url = reverse('queries-list')
     keywords = random_keyword_set()
     data = {
@@ -20,3 +20,17 @@ def test_incorrenct_intention_choice(client):
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.content == b'{"intention":["\\"TS\\" is not a valid choice."]}'
+
+
+@pytest.mark.django_db
+def test_bad_request_without_keywords(client):
+    url = reverse('queries-list')
+    data = {
+        'intention': random.choice(Query.INTENTION_CHOICES),
+        'intersect_keywords': random.choice([True, False]),
+        'public' : random.choice([True, False])
+    }
+    response = client.post(url, data, format='json')
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.content == b'{"keywords":["This field is required."]}'
